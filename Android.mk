d302f5aa6cf8a4f39e44f2b97dcaf2d5b4895240
Vitaliy Tomin <highwaystar.ru@gmail.com>
2019-01-21 11:06:11 +0800

    yellowstone: Add initial blobs
    * add blobs required for booting: graphics blobbs, its dependencies and
    firmwares
    
    * from google/tango/yellowstone:4.4.2/KOT49H.161129/161129002:user/release-keys

--- Android.mk
+++ Android.mk
@@ -18,4 +18,15 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),yellowstone)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := libnvos
+LOCAL_MODULE_OWNER := google
+LOCAL_SRC_FILES := proprietary/vendor/lib/libnvos.so
+LOCAL_MULTILIB := 32
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_PREBUILT)
+
 endif
